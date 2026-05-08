100294a35e64f5d935aeb571e61a68745b94eef8
Martin Frei <martin.frei@aon.at>
2026-05-08 17:44:39 +0200

    added pin-assignment, added hardware.c

---
+++ main/hardware.c
@@ -0,0 +1,22 @@
+#include "hardware.h"
+
+#include "driver/gpio.h"
+
+// Pin-Defines
+#define PIN_RELAIS_1
+#define PIN_RELAIS_2
+#define PIN_RELAIS_3
+#define PIN_RELAIS_4
+#define PIN_RELAIS_5
+#define PIN_RELAIS_6
+#define PIN_RELAIS_7
+#define PIN_RELAIS_8
+
+#define PIN_TEMP_MPPT
+#define PIN_TEMP_PSU
+
+#define PIN_PWM_FAN
+
+#define PIN_BTN_INTERRUPT
+
+
